790bea0c8d141cc00e89c7b1af7832e4659e6d83
CertIanS <irsnider13@gmail.com>
2026-09-17 15:38:00 -0400

    Disabled button cursor and naming error

--- main.js
+++ main.js
@@ -4,57 +4,57 @@ var monthBtn = document.getElementById("monthlyButton");
 var jsonData = null;
 
 dailyBtn.addEventListener("click", function(e){
+    dailyBtn.disabled = true;
+    weekBtn.disabled = false;
+    monthBtn.disabled = false;
     document.getElementById("workCurrent").textContent = jsonData[0].timeframes.daily.current + "hrs";
     document.getElementById("workPrev").textContent = "Yesterday - " + jsonData[0].timeframes.daily.previous + "hrs";
     document.getElementById("playCurrent").textContent = jsonData[1].timeframes.daily.current + "hr";
     document.getElementById("playPrev").textContent = "Yesterday - " + jsonData[1].timeframes.daily.previous + "hrs";
     document.getElementById("studyCurrent").textContent = jsonData[2].timeframes.daily.current + "hrs";
     document.getElementById("studyPrev").textContent = "Yesterday - " + jsonData[2].timeframes.daily.previous + "hr";
-    document.getElementById("exerciseCurr").textContent = jsonData[3].timeframes.daily.current + "hr";
+    document.getElementById("exerciseCurrent").textContent = jsonData[3].timeframes.daily.current + "hr";
     document.getElementById("exercisePrev").textContent = "Yesterday - " + jsonData[3].timeframes.daily.previous + "hr";
-    document.getElementById("socialCurr").textContent = jsonData[4].timeframes.daily.current + "hr";
+    document.getElementById("socialCurrent").textContent = jsonData[4].timeframes.daily.current + "hr";
     document.getElementById("socialPrev").textContent = "Yesterday - " + jsonData[4].timeframes.daily.previous + "hrs";
     document.getElementById("selfCurrent").textContent = jsonData[5].timeframes.daily.current + "hrs";
     document.getElementById("selfPrev").textContent = "Yesterday - " + jsonData[5].timeframes.daily.previous + "hr";
-    dailyBtn.disabled = true;
-    weekBtn.disabled = false;
-    monthBtn.disabled = false;
 });
 
 weekBtn.addEventListener("click", function(e){
+    dailyBtn.disabled = false;
+    weekBtn.disabled = true;
+    monthBtn.disabled = false;
     document.getElementById("workCurrent").textContent = jsonData[0].timeframes.weekly.current + "hrs";
     document.getElementById("workPrev").textContent = "Last week - " + jsonData[0].timeframes.weekly.previous + "hrs";
     document.getElementById("playCurrent").textContent = jsonData[1].timeframes.weekly.current + "hrs";
     document.getElementById("playPrev").textContent = "Last week - " + jsonData[1].timeframes.weekly.previous + "hrs";
     document.getElementById("studyCurrent").textContent = jsonData[2].timeframes.weekly.current + "hrs";
     document.getElementById("studyPrev").textContent = "Last week - " + jsonData[2].timeframes.weekly.previous + "hrs";
-    document.getElementById("exerciseCurr").textContent = jsonData[3].timeframes.weekly.current + "hrs";
+    document.getElementById("exerciseCurrent").textContent = jsonData[3].timeframes.weekly.current + "hrs";
     document.getElementById("exercisePrev").textContent = "Last week - " + jsonData[3].timeframes.weekly.previous + "hrs";
-    document.getElementById("socialCurr").textContent = jsonData[4].timeframes.weekly.current + "hrs";
+    document.getElementById("socialCurrent").textContent = jsonData[4].timeframes.weekly.current + "hrs";
     document.getElementById("socialPrev").textContent = "Last week - " + jsonData[4].timeframes.weekly.previous + "hrs";
     document.getElementById("selfCurrent").textContent = jsonData[5].timeframes.weekly.current + "hrs";
     document.getElementById("selfPrev").textContent = "Last week - " + jsonData[5].timeframes.weekly.previous + "hrs";
-    dailyBtn.disabled = false;
-    weekBtn.disabled = true;
-    monthBtn.disabled = false;
 });
 
 monthBtn.addEventListener("click", function(e){
+    dailyBtn.disabled = false;
+    weekBtn.disabled = false;
+    monthBtn.disabled = true;
     document.getElementById("workCurrent").textContent = jsonData[0].timeframes.monthly.current + "hrs";
     document.getElementById("workPrev").textContent = "Last month - " + jsonData[0].timeframes.monthly.previous + "hrs";
     document.getElementById("playCurrent").textContent = jsonData[1].timeframes.monthly.current + "hrs";
     document.getElementById("playPrev").textContent = "Last month - " + jsonData[1].timeframes.monthly.previous + "hrs";
     document.getElementById("studyCurrent").textContent = jsonData[2].timeframes.monthly.current + "hrs";
     document.getElementById("studyPrev").textContent = "Last month - " + jsonData[2].timeframes.monthly.previous + "hrs";
-    document.getElementById("exerciseCurr").textContent = jsonData[3].timeframes.monthly.current + "hrs";
+    document.getElementById("exerciseCurrent").textContent = jsonData[3].timeframes.monthly.current + "hrs";
     document.getElementById("exercisePrev").textContent = "Last month - " + jsonData[3].timeframes.monthly.previous + "hrs";
-    document.getElementById("socialCurr").textContent = jsonData[4].timeframes.monthly.current + "hrs";
+    document.getElementById("socialCurrent").textContent = jsonData[4].timeframes.monthly.current + "hrs";
     document.getElementById("socialPrev").textContent = "Last month - " + jsonData[4].timeframes.monthly.previous + "hrs";
     document.getElementById("selfCurrent").textContent = jsonData[5].timeframes.monthly.current + "hrs";
     document.getElementById("selfPrev").textContent = "Last month - " + jsonData[5].timeframes.monthly.previous + "hrs";
-    dailyBtn.disabled = false;
-    weekBtn.disabled = false;
-    monthBtn.disabled = true;
 });
 
 function fetchJSONData(){
